012368cb0c3782d89f575e9bad9be8973e367de7
gunasekaran2612 <{gunasekaran}@gmail.com>
2022-05-01 16:56:10 +0530

    New Change

--- src/test/java/tataelxsi/co/in/GitExample.java
+++ src/test/java/tataelxsi/co/in/GitExample.java
@@ -2,17 +2,19 @@ package tataelxsi.co.in;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GitExample {
-public static void main(String[] args) {
-	WebDriverManager.chromedriver().setup();
-	WebDriver driver = new ChromeDriver();
-	driver.get("https://google.com");
-	driver.close();
+	@Test
+	public void test() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		driver.close();
+
+
+	}
 	
-	
-	
-}
 }
