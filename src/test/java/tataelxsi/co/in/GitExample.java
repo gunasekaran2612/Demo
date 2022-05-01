e24c16add711b7fcb624c9ecc1c0fc2d363cbc83
gunasekaran2612 <{gunasekaran}@gmail.com>
2022-05-01 12:00:25 +0530

    add file

---
+++ src/test/java/tataelxsi/co/in/GitExample.java
@@ -0,0 +1,18 @@
+package tataelxsi.co.in;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GitExample {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://google.com");
+	driver.close();
+	
+	
+	
+}
+}
